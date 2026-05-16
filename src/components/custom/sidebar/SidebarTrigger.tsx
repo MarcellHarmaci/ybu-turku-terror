39b4b2d293da29f2b770346a6b845c771f5ac973
Marcell Harmaci <harmaci.marci@gmail.com>
2026-05-16 20:26:29 +0300

    Fix sidebar trigger icon state on mobile

--- src/components/custom/sidebar/SidebarTrigger.tsx
+++ src/components/custom/sidebar/SidebarTrigger.tsx
@@ -5,14 +5,14 @@ import {
 } from "@tabler/icons-react"
 
 export function SidebarTrigger() {
-  const { toggleSidebar, open } = useSidebar()
+  const { toggleSidebar, open, isMobile } = useSidebar()
 
   return (
     <button className="m-2" onClick={toggleSidebar}>
-      {open ? (
-        <IconLayoutSidebarLeftCollapseFilled size={24} />
-      ) : (
+      {!open || isMobile ? (
         <IconLayoutSidebarLeftExpandFilled size={24} />
+      ) : (
+        <IconLayoutSidebarLeftCollapseFilled size={24} />
       )}
     </button>
   )
