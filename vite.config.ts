bb6f346605f49d2a8d3861bbb0a89792d83bdd71
Marcell Harmaci <harmaci.marci@gmail.com>
2026-06-05 08:23:36 +0300

    Improve chunk sizes

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,54 @@
-import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+import path from "path"
+import { visualizer } from "rollup-plugin-visualizer"
 import { defineConfig } from "vite"
+
+const chunkDef: Record<string, string[]> = {
+  "vendor-editor": ["reactjs-tiptap-editor"],
+  "vendor-tiptap": ["tiptap"],
+  "vendor-zod": ["zod"],
+  "vendor-firebase": ["firebase"],
+  "vendor-katex": ["katex"],
+  "vendor-react-radix": [
+    "node_modules/react",
+    "node_modules/react-dom",
+    "node_modules/@radix-ui",
+  ],
+}
+
+function manualChunks(id: string) {
+  if (!id.includes("/node_modules/")) return
+
+  for (const [chunkName, modules] of Object.entries(chunkDef)) {
+    if (modules.some((module) => id.includes(module))) {
+      return chunkName
+    }
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      filename: "dist/stats.html",
+      open: true,
+      gzipSize: true,
+      brotliSize: true,
+    }),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: manualChunks,
+      },
     },
   },
 })
