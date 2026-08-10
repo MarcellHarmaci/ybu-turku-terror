c709bf4a22869dabf34942ee183c02a9b07ff51a
Marcell Harmaci <harmaci.marci@gmail.com>
2026-08-10 19:24:28 +0300

    Add error handling to csv tables

--- src/components/custom/CsvTable.tsx
+++ src/components/custom/CsvTable.tsx
@@ -3,15 +3,24 @@ import { useUrl } from "@/service/url/useUrl"
 import Papa from "papaparse"
 import { useEffect, useState } from "react"
 import { Spinner } from "../ui/spinner"
+import Alert from "./Alert"
 
 interface CsvTableProps {
   urlId: string
 }
 
 export function CsvTable({ urlId }: CsvTableProps) {
-  const { url: csvUrl, isLoading } = useUrl(urlId)
+  const { url: csvUrl, isLoading, error: urlFetchError } = useUrl(urlId)
   const [data, setData] = useState<string[][]>()
-  // TODO state for errors: ParseError[];
+  const [csvError, setCsvError] = useState<"download" | "parse" | undefined>()
+
+  const errorDescription = urlFetchError
+    ? "Failed to fetch CSV URL."
+    : csvError === "download"
+      ? "Failed to load CSV data."
+      : csvError === "parse"
+        ? "Failed to parse CSV data. The CSV format is incorrect."
+        : "An unknown error occurred."
 
   useEffect(() => {
     if (!csvUrl) {
@@ -20,41 +29,48 @@ export function CsvTable({ urlId }: CsvTableProps) {
 
     Papa.parse<string[]>(csvUrl, {
       download: true,
-      complete: function (results) {
+      error: () => setCsvError("download"),
+      complete: (results) => {
+        if (results.errors.length > 0) {
+          setCsvError("parse")
+        }
         setData(results.data)
-        // TODO set error state
       },
     })
   }, [csvUrl])
 
-  if (isLoading || !data) {
+  if (isLoading) {
     return (
       <div className="flex flex-row items-center gap-2">
         <Spinner />
-        {!data ? "Parsing..." : "Loading..."}
+        Loading...
       </div>
     )
   }
 
+  if (urlFetchError || csvError) {
+    return <Alert type="error" title="Error" description={errorDescription} />
+  }
+
   return (
-    // TODO display error state
-    // {error && <Alert type="error" title="Error" description={error} />}
-    <Table>
-      <TableBody>
-        {data.map((row, rowIndex) => (
-          <TableRow
-            key={`row-${rowIndex}`}
-            className={rowIndex % 2 === 1 ? "bg-muted/50" : undefined}
-          >
-            {row.map((cell, colIndex) => (
-              <TableCell key={`row-${rowIndex}-col-${colIndex}`}>
-                {cell}
-              </TableCell>
-            ))}
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <>
+      <Table>
+        <TableBody>
+          {(data ?? []).map((row, rowIndex) => (
+            <TableRow
+              key={`row-${rowIndex}`}
+              className={rowIndex % 2 === 1 ? "bg-muted/50" : undefined}
+            >
+              {row.map((cell, colIndex) => (
+                <TableCell key={`row-${rowIndex}-col-${colIndex}`}>
+                  {cell}
+                </TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </>
   )
 }
 
